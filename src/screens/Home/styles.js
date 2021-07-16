0981b8a3f6d80926e50056daa2f893b0c58261fb
Konrad Kurzynowski <kurzynowskikonrad@gmail.com>
2021-07-16 10:07:18 -0400

    added wallpaper and basic buttons

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,51 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: 'white',
+    width: '70%',
+    marginLeft: 25,
+  },
+  button: {
+    backgroundColor: '#fff',
+    width: 175,
+    marginTop: 25,
+    marginLeft: 25,
+    height: 40,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  searchButton: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    height: 40,
+    width: '100%',
+    marginHorizontal: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  searchButton: {},
+});
+
+export default styles;
